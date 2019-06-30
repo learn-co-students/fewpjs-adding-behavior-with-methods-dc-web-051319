02ce2892a26f69f794334fb13561a0476893b8bf
errolgh <ewatsonrei@gmail.com>
2019-06-29 20:49:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+class Cat{
+  constructor(name, sex){
+      this.name = name;
+      this.sex = sex;
+  }
+    speak(){
+      return `${this.name} says meow!`
+    }
+}
+
+class Dog{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+    speak(){
+      return `${this.name} says woof!`
+    }
+}
+
+class Bird{
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+    speak(){
+      if (this.sex === "male"){
+        return `It's me! ${this.name}, the parrot!`
+      } else if (this.sex === "female"){
+        return `${this.name} says squawk!`
+      }
+    }
+}
+
+
+
+//For an instance of Bird, speak returns conditional output. If the instance of Bird is male, speak returns "It's me! name, the parrot!". If it is not male, speak returns "name says squawk!".
